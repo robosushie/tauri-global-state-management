e6c5995cd40f80949ca26e54060b49efbb9d5938
Sushant S Samuel <iam@robosushie.com>
2023-07-25 03:04:42 +0530

    Removed dead block from main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,12 +11,6 @@ mod states{
 
 use crate::states::states::subscribe_state_events;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
@@ -24,7 +18,6 @@ fn main() {
             Ok(())
         })
         .manage(states::states::set_default_state())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
